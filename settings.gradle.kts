de08f0e2a484e1b567076b69c4492b479a0b7086
Vladimir <vryabchenok@gmail.com>
2021-03-08 14:53:36 +0300

    changing applying of versions in settings gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,9 @@ pluginManagement {
     val kotlinVersion: String by settings
 
     plugins {
-        kotlin("multiplatform") version kotlinVersion
-        kotlin("jvm") version kotlinVersion
-        kotlin("js") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion apply false
+        kotlin("jvm") version kotlinVersion apply false
+        kotlin("js") version kotlinVersion apply false
         kotlin("plugin.serialization") version kotlinVersion apply false
     }
 }
