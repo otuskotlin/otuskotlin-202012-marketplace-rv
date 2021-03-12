7fef289883d1182d2b32e10fec5e96012d42391b
rumoroka <vryabchenok@gmail.com>
2021-03-12 14:10:41 +0300

    updating models - adding conection between arts and workshops

---
+++ ok-marketplace-rv-app-ktor/src/main/kotlin/ru/otus/otuskotlin/Application.kt
@@ -0,0 +1,86 @@
+package ru.otus.otuskotlin
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.features.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+/**
+ * Please note that you can use any other name instead of *module*.
+ * Also note that you can have more then one modules in your application.
+ * */
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+    install(Locations) {
+    }
+
+
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+    routing {
+        get<MyLocation> {
+            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+        }
+        // Register nested routes
+        get<Type.Edit> {
+            call.respondText("Inside $it")
+        }
+        get<Type.List> {
+            call.respondText("Inside $it")
+        }
+    }
+    routing {
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+    }
+    routing {
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+    }
+}
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+@Location("/type/{name}")
+data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
